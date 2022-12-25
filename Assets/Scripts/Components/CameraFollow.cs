4742c8e0581f959475fdddae5c554c7fa8f5a431
Александр Мельников <meln.alex97@gmail.com>
2022-12-26 00:22:04 +0700

    Camera smooth follow
    Добавление плавного перемещения камеры за наблюдаемым объектом

---
+++ Assets/Scripts/Components/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using PixelGame.View;
+using UnityEngine;
+
+namespace PixelGame.Components
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraFollow:MonoBehaviour
+    {
+        [SerializeField] private LevelObjectView _followTarget;
+        [SerializeField] private float _xOffset = 0f;
+        [SerializeField] private float _smoothTime = 0.25f;
+
+        private Vector3 _velocity = Vector3.zero;
+
+        private void FixedUpdate()
+        {
+            if (!_followTarget) return;
+
+            var targetPos = new Vector3(_followTarget.transform.position.x + _xOffset, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, _smoothTime);
+        }
+    }
+}

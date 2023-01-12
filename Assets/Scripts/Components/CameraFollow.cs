74cf22fcaaf3a5f6ec2ba0d5436fc67b07b1cd54
SouJl <meln.alex97@gmail.com>
2023-01-12 12:32:56 +0700

    Add camera Y clamp and level update

--- Assets/Scripts/Components/CameraFollow.cs
+++ Assets/Scripts/Components/CameraFollow.cs
@@ -8,7 +8,10 @@ namespace PixelGame.Components
     {
         [SerializeField] private LevelObjectView _followTarget;
         [SerializeField] private float _levelLength = 100;
+        [SerializeField] private float _maxY = 10;
+        [SerializeField] private float _minY = -10;
         [SerializeField] private float _xOffset = 0f;
+        [SerializeField] private float _yOffset = 0f;
         [SerializeField] private float _smoothTime = 0.25f;
 
         private Vector3 _velocity = Vector3.zero;
@@ -17,8 +20,9 @@ namespace PixelGame.Components
         {
             if (!_followTarget) return;
 
-            var clampPos = Mathf.Clamp(_followTarget.transform.position.x + _xOffset, 0, _levelLength);
-            var targetPos = new Vector3(clampPos, transform.position.y, transform.position.z);
+            var clampPosX = Mathf.Clamp(_followTarget.transform.position.x + _xOffset, 0, _levelLength);
+            var clampPosY = Mathf.Clamp(_followTarget.transform.position.y + _yOffset, _minY, _maxY);
+            var targetPos = new Vector3(clampPosX, clampPosY, transform.position.z);
             transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, _smoothTime);
         }
     }

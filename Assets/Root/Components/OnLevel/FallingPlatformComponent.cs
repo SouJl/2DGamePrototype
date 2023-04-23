ee7d3e0b70f03ceaa8461e3f455aab019b24317a
Александр Мельников <meln.alex97@gmail.com>
2023-04-23 23:18:31 +0700

    Add Shake and Fall platform and fix some visual bugs

--- Assets/Root/Components/OnLevel/FallingPlatformComponent.cs
+++ Assets/Root/Components/OnLevel/FallingPlatformComponent.cs
@@ -9,11 +9,39 @@ namespace Assets.Root.Components.OnLevel
         [SerializeField] private float _fallingDelay = 1f;
         [SerializeField] private float _destroyDelay = 3f;
 
+        [SerializeField] private bool _isShakeAcive = true;
+        [SerializeField] private float _timeBetweenCallShake = 1f;
+        [SerializeField] private ShakeComponent[] _shakeComponents;
+
+        private float _lastShakeTime = 0f;
+
         private Rigidbody2D _rigidbody;
+  
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            
+            _lastShakeTime = _timeBetweenCallShake;
+        }
+
+        private void Update()
+        {
+            if (_isShakeAcive)
+            {
+                if (_lastShakeTime > _timeBetweenCallShake)
+                {
+                    foreach (var shakeComponent in _shakeComponents)
+                    {
+                        shakeComponent.Begin();
+                    }
+                    _lastShakeTime = 0;
+                }
+                else
+                {
+                    _lastShakeTime += Time.deltaTime;
+                }
+            }        
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
@@ -26,7 +54,12 @@ namespace Assets.Root.Components.OnLevel
 
         IEnumerator Falling()
         {
-            yield return new WaitForSeconds(_fallingDelay);
+            _isShakeAcive = false;
+            foreach (var shakeComponent in _shakeComponents)
+            {
+                shakeComponent.End();
+            }
+            yield return new WaitForSeconds(_fallingDelay); 
             _rigidbody.bodyType = RigidbodyType2D.Dynamic;
             Destroy(gameObject, _destroyDelay);
         }
